49a27ee6401bde40eae6d353d23b05028f56d2a3
AlexUstav22 <c-g-1990@mail.ru>
2024-09-28 01:02:11 +0700

    adding work logic

--- calculator.py
+++ calculator.py
@@ -23,3 +23,14 @@ choice = input("Введите номер операции (1/2/3/4): ")
 
 num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
+
+if choice == '1':
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+elif choice == '2':
+    print(f"{num1} – {num2} = {subtract(num1, num2)}")
+elif choice == '3':
+    print(f"{num1} * {num2} = {multiply(num1, num2)}")
+elif choice == '4':
+    print(f"{num1} / {num2} = {divide(num1, num2)}")
+else:
+    print("Неверный ввод")
